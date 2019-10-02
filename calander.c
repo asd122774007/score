6d14ba944cbf546e0dff5c49517e593f57d18be5
asd122774007 <56062242+asd122774007@users.noreply.github.com>
2019-10-02 15:05:12 +0800

    Update calander.c

--- calander.c
+++ calander.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 int main(void) {
-	printf("sun mon tue wed thu fri sat\n");    //印出星期
-	int month = 30;
-	int day = 1;
-	int week = 3;
-	int k;
-	for(k=3;k>0;--k)  printf("    ");
-	while(day<=month){
-		if(week == 7){               //七天換行一次
-			printf("\n");
-			week = 0;
-		}
-		printf("%-4d",day);        //印出日子
-		++day;
-		++week;
-	}
-	return 0;
+    printf("sun mon tue wed thu fri sat\n");
+    int month = 30;   //一個月有幾天 
+    int day;          //已經印到第幾天了
+    int week;         //已經印到這個星期的第幾天了
+    int k;
+    for(k=3;k>0;--k)  printf("    ");
+    for(day=1, week=3; day<=month; ++day, ++week){
+        if(week > 7){               //七天換行一次 
+            printf("\n");
+            week = 1;
+        }
+        printf("%-4d",day);        //印出日子
+    }   
+    printf("\n");
+    return 0;
 }
